cfa1cabf872e881f83c413849bbf906295a3387c
zinhart <aromzinhartdegraca@gmail.com>
2023-04-25 10:25:52 -0700

    Minor updates for publishing

--- Publish.ps1
+++ Publish.ps1
@@ -1,13 +1,13 @@
 # This assumes you are running PowerShell 5
 
 # Parameters for publishing the module
-$ModuleName='PoshWebSocketClient'
+$ModuleName='PwshWebSocketClient'
 $Path = ".\$ModuleName"
 $PublishParams = @{
     NuGetApiKey = Get-Content .env # Swap this out with your API key
     Path = $Path
     ProjectUri = "https://github.com/zinhart/$ModuleName"
-    Tags = @('Websockets', '', 'REST', 'API' )
+    Tags = @('Websockets', 'API' )
 }
 
 # We install and run PSScriptAnalyzer against the module to make sure it's not failing any tests
