a8ec47501cb7fcf7162e43bdebc6c3b4777cfebc
zinhart <aromzinhartdegraca@gmail.com>
2023-04-28 04:39:36 -0700

    Updating test-proxy script

--- tests/test-proxy.ps1
+++ tests/test-proxy.ps1
@@ -1,6 +1,9 @@
+#start proxy: python .\tests\proxy.py --port 8001 --remote_url ws://localhost:8000
+#start intended receiver: python .\tests\echo-server.
 Import-Module -Name '.\PwshWebSocketClient'
-Connect-Websocket -Uri 'ws://localhost:8000/' -Proxy 'http://localhost:8080/'
+Connect-Websocket -Uri 'ws://localhost:8001/' -Proxy 'ws://localhost:8000/'
 Send-Message -Message 'Applesauce'
 Receive-Message 
 Get-WebsocketState
+Disconnect-Websocket
 Remove-Module PwshWebSocketClient
