129193e6e78b7c468d40b958d017ebebb7be7013
zinhart <aromzinhartdegraca@gmail.com>
2023-04-30 15:32:00 -0700

    Update to instructions on how to import this into the trusted root certificate store on windows

--- tests/test-send-receive-wss.ps1
+++ tests/test-send-receive-wss.ps1
@@ -1,3 +1,7 @@
+<#
+  In order for this to work on windows, we must generate a self-signed cert with .\tests\New-Pem.ps1 and
+  then move the cert into the trust root certificate store
+#>
 Import-Module -Name '.\PwshWebSocketClient'
 Connect-Websocket -Uri 'wss://localhost:8001' -Certificate 'C:\Users\zinhart\Documents\code\PoshWebSocketClient\tests\localhost.cer'
 Send-Message -Message 'Applesauce'
